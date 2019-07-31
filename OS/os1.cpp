fab098cc1a1af9ee6677f6063ec1f1b6dc67b65d
Chaitanya Raj <chaitanya@fx505dy.ib-wrb302n.setup.cn>
2019-07-31 17:04:22 +0530

    home

---
+++ OS/os1.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<sys/vfs.h>
+#include<sys/sysinfo.h>
+#include<sys/utsname.h>
+
+int main()
+{
+	int MB,GB,p,q,r;
+	struct sysinfo s1;
+	struct utsname s2;
+	struct statfs s3;
+	r=sysinfo(&s1);
+	q=uname(&s2);
+	p=statfs("./",&s3);
+	MB=(1024*1024);
+	GB=(MB*1024);
+	if(r==0&&q==0&p==0)
+	{
+		printf("\nSystem name:%s",s2.sysname);
+		printf("\nKernal version: %s",s2.version);
+		printf("\nRelease : %s",s2.release);
+		printf("\nMachine : %s",s2.machine);
+		printf("\nTotal ram: %ldmb",(s1.totalram/MB));
+		printf("\nFree ram : %ldmb",(s1.freeram/MB));
+		printf("\nUsed ram:% ldmb",(s1.totalram-s1.freeram)/MB);
+		printf("\nTotalsize of partition: %ldGB",(s3.f_bsize*s3.f_blocks)/GB);	
+	} 
+	else
+	{printf("error");
+	}
+	return 0;
+}
+
