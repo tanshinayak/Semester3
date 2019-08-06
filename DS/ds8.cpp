5972cdc6f008d28bd307968977b42cdde13e0df2
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2019-08-06 17:16:34 +0530

    ds8final

--- DS/ds8.cpp
+++ DS/ds8.cpp
@@ -6,52 +6,60 @@ using namespace std;
 
 int main()
 {
+    char choice;
+    do{
     string num1,num2,result;
     cout<<"\nEnter first number : ";
     cin>>num1;
+    int l1=num1.length();
     // getline(cin,num1);
     cout<<"\nEnter second number : ";
     cin>>num2;
+    int l2=num2.length();
     // getline(cin,num2);
-    Stack<char> s1(10),s2(10),s3(11);
-    for(int i=0;num1[i]!='\0';i++)
+    Stack<int> s1(l1),s2(l2),s3(l1+l2);
+    for(int i=0;i<l1;i++)
     {
-        s1.push(num1[i]);
+        s1.push(num1[i]-48);
     }
-    for(int i=0;num2[i]!='\0';i++)
+    for(int i=0;i<l2;i++)
     {
-        s2.push(num2[i]);
+        s2.push(num2[i]-48);
     }
     int carry=0;
-    while(!s1.isempty() && !s2.isempty())
+    while(!s1.isempty() || !s2.isempty())
     {
         int x,y;
         if(s1.isempty())
             x = 0;
         else
-            x = s1.pop()-48;
+            x = s1.pop();
         if(s2.isempty())
             y = 0;
         else
-            y = s2.pop()-48;
+            y = s2.pop();
         int z = x+y+carry;
-        s3.push(z%10+48);
-        if(z>10){
-            carry=1;
+        if(z>=10){
+            carry=z/10;
+            s3.push(z%10);
         }
         else
         {
             carry=0;
+            s3.push(z);
         }
     }
-    if(carry==1)
+    if(carry>0)
     {
-        s3.push(49);
+        s3.push(carry);
     }
     while(!s3.isempty())
     {
-        char a = s3.pop();
+        char a = s3.pop()+48;
         result+=a;
     }
     cout<<"\nSum = "<<result;
+    cout<<"\nContinue?(y/n)";
+    cin>>choice;
+}while(choice == 'y');
 }
