569f7e78a1a638d9d2f846a2ec7214f12728b855
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2019-08-19 11:53:59 +0530

    os3 and os4

---
+++ OS/os4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<sys/types.h>
+#include<unistd.h>
+using namespace std;
+
+int main()
+{
+    pid_t pid;
+    pid = fork();
+    cout<<"1pid is "<<pid;
+    cout<<endl<<"1get_pid is "<<getpid()<<endl;
+    if(pid<0)
+    {
+        cout<<endl<<"this is a process ... "<<endl;
+        cout<<"2pid is "<<pid;
+        cout<<endl<<"2getpid is "<<getpid()<<endl;
+    }
+    else
+    {
+        cout<<endl<<"process continues..."<<endl;
+        cout<<"3pid is "<<pid;
+        cout<<endl<<"3getpid is "<<getpid()<<endl;
+    }
+    return 0;
+}
