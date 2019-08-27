f1ef18da197ed551e6df62554a236a7d38988e5e
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2019-08-27 09:58:15 +0530

    blahblah

---
+++ AP/ColorChanger/app/src/main/java/com/example/dsc2/colorchanger/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dsc2.colorchanger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onClick()
+}
