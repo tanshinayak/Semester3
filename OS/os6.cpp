a948dbc46a35076ccead39160153eeaa6d7b4ba0
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2019-09-02 11:49:56 +0530

    os6

--- OS/os6.cpp
+++ OS/os6.cpp
@@ -5,15 +5,28 @@ using namespace std;
 
 int main()
 {
+    pid_t pid;
     int msize = 20;
-    char msg[] = "Message Passing";
+    char msg[] = "Message Passing\n";
     char buf[msize];
     int p[2];
     int a = pipe(p);
     if (a < 0)
         exit(1);
-    write(p[1], msg, msize);
-    read(p[0], msg, msize);
+    pid = fork();
+    if (pid == 0)
+    {
+        read(p[0], buf, msize);
+        // cout << "Child Process " << pid;
+        // close(p[0]);
+    }
+    else
+    {
+        write(p[1], msg, msize);
+        // cout << "Parent Process " << pid;
+        // // close(p[1]);
+    }
+
     cout << buf << endl;
     return 0;
 }
